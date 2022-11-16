64c210962966522e28a970f175df058219665429
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-16 13:06:35 +1100

    trying to digest project idea

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
+/**
+ * Traffic intersection
+ * Give parameters to calculate red light times
+ *
+ * @author - Wout Vinckevleugel (Wovi10)
+ */
 fun main() {
-
+    // TODO Traffic light time calculators
+    
 }
