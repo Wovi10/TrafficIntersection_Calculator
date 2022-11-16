f3fa5c2f6866d0d5d9a3bb06451dedf688fe81bd
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-16 13:10:59 +1100

    Added markdown for easier digesting and rules

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,2 +1,12 @@
+/**
+ * Calculator
+ * This class calculates a few red light timings depending on input
+ *
+ * @author - Wout Vinckevleugel (Wovi10)
+ */
 class Calculator {
+    // TODO fun calculate
+    /*
+    *
+    */
 }
