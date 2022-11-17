b6801dc4e9da22064956cd5c1bb335f36c8140d8
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-17 16:50:04 +1100

    Update Functions.kt

--- src/main/kotlin/utils/Functions.kt
+++ src/main/kotlin/utils/Functions.kt
@@ -1,7 +1,7 @@
 package utils
 
 object Functions {
-    fun <T> printArray(arrayToPrint: Array<T>){
+    fun <T> printArray(arrayToPrint: Array<T>) {
         var output: String = Constants.EMPTY_STRING
         var counter = Constants.ONE
         for (element in arrayToPrint) {
@@ -13,7 +13,7 @@ object Functions {
         println(output)
     }
     
-    fun <T> printArrayList(arrayListToPrint: ArrayList<T>){
+    fun <T> printArrayList(arrayListToPrint: ArrayList<T>) {
         var output: String = Constants.EMPTY_STRING
         var counter = Constants.ONE
         for (element in arrayListToPrint) {
