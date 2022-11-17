72fa8d382c715f27ed3bf0a07e6d76fe2d3ac230
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-17 15:17:05 +1100

    Create IntersectionTest.kt

---
+++ src/test/kotlin/intersection/IntersectionTest.kt
@@ -0,0 +1,18 @@
+package intersection
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class IntersectionTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    fun tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun calculateThroughTime() {
+    }
+}
