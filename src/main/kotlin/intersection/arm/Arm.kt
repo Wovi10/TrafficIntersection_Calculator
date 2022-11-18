61ebe596f0376f75b856c0bf9e0adcf0f7c0307e
Wout Vinckevleugel <50151315+Wovi10@users.noreply.github.com>
2022-11-18 16:21:06 +1100

    Made implementation more readable

--- src/main/kotlin/intersection/arm/Arm.kt
+++ src/main/kotlin/intersection/arm/Arm.kt
@@ -1,7 +1,7 @@
 package intersection.arm
 
 import intersection.arm.lane.Lane
-import intersection.arm.lane.LaneUsage.*
+import intersection.arm.lane.LaneUsage
 import intersection.stage.light.Light
 import utils.Constants.DEFAULT_ARM_SPEED
 import utils.Constants.DEFAULT_ARM_WIDTH
@@ -9,7 +9,6 @@ import utils.Constants.DEFAULT_DOUBLE
 import utils.Constants.DEFAULT_INPUT_LANES_NUM
 import utils.Constants.DEFAULT_OUTPUT_LANES_NUM
 import utils.Constants.ONE
-import utils.Constants.TWO
 import utils.Constants.ZERO
 
 class Arm constructor(
@@ -45,30 +44,17 @@ class Arm constructor(
 
     private fun initLanes(): ArrayList<Lane> {
         val output: ArrayList<Lane> = ArrayList()
-        var counter = ONE
-        repeat (numLanes){
-            val laneToAdd: Lane
-            when (counter) {
-                ONE -> {
-                    laneToAdd = Lane(Left)
-                    counter++
-                }
-                TWO -> {
-                    laneToAdd = Lane(Straight)
-                    counter++
-                }
-                else -> {
-                    laneToAdd = Lane(Right)
-                    counter = ONE
-                }
-            }
+        var counter = ZERO
+        repeat(numLanes) {
+            val laneToAdd = Lane(LaneUsage.values()[counter])
+            if (counter == inputLanesNum - ONE) counter = ZERO else counter++
             output.add(laneToAdd)
         }
         return output
     }
 
     fun getLights(): Array<Light> {
-        val lights: Array<Light> = Array(numLanes){i ->
+        val lights: Array<Light> = Array(numLanes) { i ->
             lanes[i].light
         }
         return lights
